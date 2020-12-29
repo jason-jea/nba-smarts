bdff6907bae00082f4bebfe60d32e1806fcadde4
Jason Jea <jasonzjea@gmail.com>
2020-12-28 23:43:08 -0600

    remove loading libraries

--- R/plumbing.R
+++ R/plumbing.R
@@ -1,16 +1,9 @@
-library(httr)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(jsonlite)
-library(lubridate)
-
 #' Generic function handling GET requests to stats.nba.com endpoints.
 #' Note that headers are set to mimic https://www.nba.com on a browser making requests to load stats.
 #' @param endpoint name of stats.nba.com endpoint
 #' @param params named vector containing parameters
 #' @return http GET response object contain the resource (aka endpint) name, list of parameters, and a list of results
-#' @examples 
+#' @examples
 #' \dontrun{
 #' parameters <- c(
 #'   "DateFrom"="",
@@ -37,16 +30,16 @@ library(lubridate)
 #'   "VsConference"="",
 #'   "VsDivision"=""
 #' )
-#' 
+#'
 #' endpoint_str <- "playerdashboardbyyearoveryear"
-#' 
+#'
 #' .stats_request(endpoint = endpoint_str, params = parameters)
 #' }
 .stats_request <- function(endpoint, params) {
   url <- "https://stats.nba.com/stats/"
   param_str = paste0(paste0(names(params), "=", params), collapse = "&")
   url <- paste0(url, endpoint, "?", param_str)
-  
+
   headers <- c(
     `Host` = 'stats.nba.com',
     `User-Agent` = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv =72.0) Gecko/20100101 Firefox/72.0',
@@ -60,21 +53,21 @@ library(lubridate)
     `Pragma` = 'no-cache',
     `Cache-Control` = 'no-cache'
   )
-  
+
   response <- fromJSON(content(GET(url, add_headers(headers)), as = "text"))
-  
+
   return(response)
 }
 
 .unpack_stats_request <- function(response) {
   stage_df <- response$resultSets
-  
+
   # stats.nba.com request reponse is a dataframe of dataframes, where each row represents a dataset.
   # This is because a request could return multiple TYPES of datasets, such as stats by year, and then also a career total dataset.
   # The columns seem to be consistent:
   # 1) column of vectors named "name", representing the name of the dataset
   # 2) column of vector lists named "headers", where each element is a vector of characters that map to column headers for that particular dataset.
-  # 3) column of 
+  # 3) column of
   final_df <- pmap_df(stage_df, function(name, headers, rowSet){
     if (length(rowSet) == 0) {
       dataset <- data.frame(t(headers), stringsAsFactors = FALSE)
@@ -83,12 +76,12 @@ library(lubridate)
     }
     else {
       dataset <- data.frame(rowSet, stringsAsFactors = FALSE)
-      colnames(dataset) <- headers  
+      colnames(dataset) <- headers
     }
-    
+
     return(dataset)
   })
-  
+
   return(final_df)
 }
 
@@ -96,7 +89,7 @@ calculate_fantasy_points <- function(df) {
   return(
     df %>%
       mutate(
-        points = as.integer(PTS)*.5 + as.integer(REB) + as.integer(AST) - as.integer(TOV) + as.integer(STL)*2 + as.integer(BLK)*2 + as.integer(DD2)*3 + as.integer(TD3)*3 
+        points = as.integer(PTS)*.5 + as.integer(REB) + as.integer(AST) - as.integer(TOV) + as.integer(STL)*2 + as.integer(BLK)*2 + as.integer(DD2)*3 + as.integer(TD3)*3
       )
   )
 }
